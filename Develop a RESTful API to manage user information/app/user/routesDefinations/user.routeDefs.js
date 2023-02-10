345c688c0eea1269da2ecdd5e488a5bd7e67ad23
fawad4bros <fawad4bros@gmail.com>
2023-02-10 23:33:34 +0500

    refactor

--- Develop a RESTful API to manage user information/app/user/routesDefinations/user.routeDefs.js
+++
@@ -1,5 +0,0 @@
-const userRoute = require("../routes/user.route");
-
-module.exports = function (app) {
-  app.use("/api/user", userRoute);
-};
--- Develop a RESTful API to manage user information/config/db.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-mongoose.set("strictQuery", false);
-const connectDB = async () => {
-  mongoose
-    .connect(process.env.MONGODB_STRING)
-    .then(() => console.log("Connected successfully"))
-    .catch((err) => console.log(err));
-};
-module.exports = connectDB;
